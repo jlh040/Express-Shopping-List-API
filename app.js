39d157c11a2c1270d928781f5b1a6ac2803d89a6
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-26 14:39:19 -0700

    created a global error handler and a 404 error handler

--- app.js
+++ app.js
@@ -25,11 +25,21 @@ const app = express();
 
 
 
+// If the URL is not matched, this middleware will throw a 404 error
+app.use((req, res, next) => {
+    const err = new ExpressError('Resource not found', 404);
+    return next(err)
+})
 
+// Global error handler
+app.use((err, req, res, next) => {
+    let status = err.status || 500;
+    let message = err.message;
 
-
-
-
-
+    res.status(status).json({
+        message,
+        status
+    })
+})
 
 module.exports = app
